8bb0032ec2ee75ae89808f07bc283b6afaad420a
ArcticaSnow <katy.shamina@yandex.ru>
2022-09-12 12:10:45 +0300

    Сommented code removed

--- 2.2.1. spring_hibernate/src/main/java/hiber/model/User.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/model/User.java
@@ -21,7 +21,6 @@ public class User {
    private String email;
 
    @OneToOne(mappedBy = "user", cascade = CascadeType.ALL)
-//   @JoinColumn(name = "car_id", referencedColumnName = "id")
    private Car car;
 
    public User() {}
