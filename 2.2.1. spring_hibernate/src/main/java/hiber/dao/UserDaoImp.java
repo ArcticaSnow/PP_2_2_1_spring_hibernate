fe8b0d6f9c028ada49e5457a7654395b58e185bb
ArcticaSnow <katy.shamina@yandex.ru>
2022-09-13 14:03:22 +0300

    Without type casting

--- 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
@@ -5,7 +5,6 @@ import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-import javax.persistence.TypedQuery;
 import java.util.List;
 
 @Repository
@@ -20,20 +19,17 @@ public class UserDaoImp implements UserDao {
     }
 
     @Override
-    @SuppressWarnings("unchecked")
     public List<User> listUsers() {
-        TypedQuery<User> query = sessionFactory.getCurrentSession().createQuery("from User");
-        return query.getResultList();
+        return sessionFactory.getCurrentSession().createQuery("from User", User.class).getResultList();
     }
 
     @Override
     public User findUser(String model, int series) {
-        User user = (User) sessionFactory.getCurrentSession()
-                .createQuery("from User where car.model =:model and car.series =:series")
-                .setParameter("model", model).setParameter("series", series)
-                .getSingleResult();
+        return sessionFactory.getCurrentSession()
+                .createQuery("from User where car.model = :model  and car.series = :series", User.class)
+                .setParameter("model", model)
+                .setParameter("series", series).getSingleResult();
 
-        return user;
     }
 
 }
